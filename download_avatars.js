56cbfa0e7a6e1f93e673b9afd534f196383102e8
Hannah <hanniesutton@gmail.com>
2019-03-11 23:50:05 +0000

    Tidied up code and added a few comments explaining what everything did

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,14 @@
 var repoOwner = process.argv.slice(2)[0];
 var repoName = process.argv.slice(2)[1];
-
 var request = require('request');
 var fs = require('fs');
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_TOKEN = require('./secrets.js')
-// console.log(GITHUB_TOKEN.GITHUB_TOKEN)
-
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if (repoOwner && repoName) {
+  if (repoOwner && repoName) { //checks that there is data in the repoOwner and repoName. If not tells user what they're doing wrong.
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
@@ -19,33 +17,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
       },
     };
 
+//loops through each user object in body and cb the function
     request(options, function(err, res, body) {
       var users = JSON.parse(body);
       for (var each of users) {
         cb(each.avatar_url, `avatars/${each.login}.jpg`)
       }
-      // cb(err, body);
     });
-
   } else {
-    console.log("variables not defined");
+    console.log("Need to enter repoOwner and repoName in node");
   }
 }
 
-
+//gets parameters from the getRepoContributors function
 function downloadImageByURL(url, filePath) {
-
   request.get(url)
-     .on('error', function (err) {                                   // Note 2
+     .on('error', function (err) {
        throw err;
      })
-     .on('response', function (response) {                           // Note 3
+     .on('response', function (response) {
       console.log('Download complete')
     })
 
   .pipe(fs.createWriteStream(filePath));
 }
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 getRepoContributors(repoOwner, repoName, downloadImageByURL)
