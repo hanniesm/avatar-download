8a9949358de93a2a9a3917831be197c8efd14617
Hannah <hanniesutton@gmail.com>
2019-03-11 22:10:05 +0000

    added authorization and parsed string to JSON and looped to get avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -6,59 +6,33 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-var token = require('/secrets.js')
+var GITHUB_TOKEN = require('./secrets.js')
+// console.log(GITHUB_TOKEN.GITHUB_TOKEN)
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
-    }
+      'User-Agent': 'request',
+      'Authorization': `token ${GITHUB_TOKEN.GITHUB_TOKEN}`
+    },
   };
 
   request(options, function(err, res, body) {
+    var users = JSON.parse(body);
+    for (var each of users) {
+      console.log(each.avatar_url)
+    }
     cb(err, body);
   });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
 
 
 
-/*var fs = require('fs');
-
-// so need it to
-
-request.get('https://avatars2.githubusercontent.com/u/16829276?s=400&va=4')
-       .on('error', function (err) {                                   // Note 2
-         throw err;
-       })
-       .on('response', function (response) {                           // Note 3
-        console.log('Response Status Code: ', response.statusCode);
-        console.log('Response Status Message: ', response.statusMessage);
-        console.log('Response Headers: ', response.header)
-        console.log('Download complete')
-       })
-       .pipe(fs.createWriteStream('./avatars'));
-*/
-// require `request` and the Node `fs` (filesystem) module
-/*var request = require('request');
-var fs = require('fs');
-
-request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-       .on('error', function (err) {                                   // Note 2
-         throw err;
-       })
-       .on('response', function (response) {                           // Note 3
-        console.log('Response Status Code: ', response.statusCode);
-        console.log('Response Status Message: ', response.statusMessage);
-        console.log('Response Headers: ', response.header)
-        console.log('Download complete')
-       })
-       .pipe(fs.createWriteStream('./future.jpg'));
-                      // Note 4
-
-*/
