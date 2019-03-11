15835c53c6fdc849b7715463d0725050bc90d172
Hannah <hanniesutton@gmail.com>
2019-03-11 21:11:53 +0000

    added token to gitignore and required it in download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -4,11 +4,23 @@
 // var url =
 
 var request = require('request');
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+var token = require('/secrets.js')
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
