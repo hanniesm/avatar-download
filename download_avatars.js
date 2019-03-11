f0557f970ddfc274b13112c8fb09e0d4a73dcf8b
Hannah <hanniesutton@gmail.com>
2019-03-11 22:59:54 +0000

    created print URL function

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,7 @@
 // var url =
 
 var request = require('request');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_TOKEN = require('./secrets.js')
@@ -29,10 +30,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-});
 
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+     .on('error', function (err) {                                   // Note 2
+       throw err;
+     })
+     .on('response', function (response) {                           // Note 3
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Download complete')
+    })
+
+  .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
